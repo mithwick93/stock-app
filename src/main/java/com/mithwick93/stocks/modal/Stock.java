ffdeecf63f84c10a5230902ff796c9cee71698b7
mithila <mithwick93@gmail.com>
2023-01-13 01:37:44 +0530

    Add Stock entity

---
+++ src/main/java/com/mithwick93/stocks/modal/Stock.java
@@ -0,0 +1,48 @@
+package com.mithwick93.stocks.modal;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+
+/**
+ * Stock entity which contains info about stock, its name and price.
+ *
+ * @author mithwick93
+ */
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "stocks")
+public class Stock {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private BigDecimal currentPrice;
+
+    private ZonedDateTime lastUpdate;
+
+    @PreUpdate
+    protected void onUpdate() {
+        lastUpdate = ZonedDateTime.now(ZoneOffset.UTC);
+    }
+}
