6a344b70edfecde5f2892e70aeb326706f95a7fa
mithila <mithwick93@gmail.com>
2023-01-13 02:19:47 +0530

    refactor StockRepository and switch to JpaRepository

--- src/main/java/com/mithwick93/stocks/dal/repository/StockRepository.java
+++ src/main/java/com/mithwick93/stocks/dal/repository/StockRepository.java
@@ -1,11 +1,12 @@
 package com.mithwick93.stocks.dal.repository;
 
 import com.mithwick93.stocks.modal.Stock;
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
  * Repository for {@link Stock} entities to perform CRUD operations.
+ *
  * @author mithwick93
  */
-public interface StockRepository extends PagingAndSortingRepository<Stock, Long> {
+public interface StockRepository extends JpaRepository<Stock, Long> {
 }
