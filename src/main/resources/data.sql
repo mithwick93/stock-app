fb46533bf372e0b4f85c1e7763b67c100c435f3a
mithila <mithwick93@gmail.com>
2023-01-14 00:54:17 +0530

    add sql database files and settings

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO stocks (id, name, current_price, last_update)
+VALUES (1, 'awesomeStock', '12.34', CURRENT_TIMESTAMP() ),
+       (2, 'jollyStock', '9.99', CURRENT_TIMESTAMP() ),
+       (3, 'bestStock', '70.45', CURRENT_TIMESTAMP() ),
+       (4, 'incredibleStock', '40', CURRENT_TIMESTAMP() );
