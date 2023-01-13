35d7b05e11c6cff8237ce49d7d41a131578856cb
mithila <mithwick93@gmail.com>
2023-01-14 03:10:12 +0530

    refactor code

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
-INSERT INTO stocks (id, name, current_price, last_update)
-VALUES (1, 'awesomeStock', '12.34', CURRENT_TIMESTAMP() ),
-       (2, 'jollyStock', '9.99', CURRENT_TIMESTAMP() ),
-       (3, 'bestStock', '70.45', CURRENT_TIMESTAMP() ),
-       (4, 'incredibleStock', '40', CURRENT_TIMESTAMP() );
+INSERT INTO stocks (name, current_price, last_update)
+VALUES ('awesomeStock', '12.34', CURRENT_TIMESTAMP),
+       ('jollyStock', '9.99', CURRENT_TIMESTAMP),
+       ('bestStock', '70.45', CURRENT_TIMESTAMP),
+       ('incredibleStock', '40', CURRENT_TIMESTAMP);
